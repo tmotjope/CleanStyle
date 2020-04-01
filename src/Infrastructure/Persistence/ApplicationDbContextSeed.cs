656838d6dd68f147eb6b463a9286b5639c982593
Teboho Motjope <tmotjope@gmail.com>
2020-04-01 08:12:34 +0200

    Initial commit

---
+++ src/Infrastructure/Persistence/ApplicationDbContextSeed.cs
@@ -0,0 +1,42 @@
+﻿using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastructure.Persistence
+{
+    public static class ApplicationDbContextSeed
+    {
+        public static async Task SeedAsync(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            // Seed, if necessary
+            if (!context.TodoLists.Any())
+            {
+                context.TodoLists.Add(new TodoList
+                {
+                    Title = "Shopping",
+                    Items =
+                    {
+                        new TodoItem { Title = "Apples", Done = true },
+                        new TodoItem { Title = "Milk", Done = true },
+                        new TodoItem { Title = "Bread", Done = true },
+                        new TodoItem { Title = "Toilet paper" },
+                        new TodoItem { Title = "Pasta" },
+                        new TodoItem { Title = "Tissues" },
+                        new TodoItem { Title = "Tuna" },
+                        new TodoItem { Title = "Water" }
+                    }
+                });
+            }
+
+            // Create default administrator
+            var defaultUser = new ApplicationUser { UserName = "administrator@localhost", Email = "administrator@localhost" };
+
+            if (userManager.Users.All(u => u.Id != defaultUser.Id))
+            {
+                await userManager.CreateAsync(defaultUser, "Administrator1!");
+            }
+        }
+    }
+}
